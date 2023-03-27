e639390b3b8f15fbf87813af894aaba2b74250d1
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-03-27 14:48:01 -0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
-temp
+const form = document.querySelector('form');
+const loginButton = document.querySelector('#login-button');
+
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+  if (username === 'user' && password === 'password') {
+    alert('Login successful!');
+  } else {
+    alert('Incorrect username or password.');
+  }
+});
