51210826438f2ccf9ddf807273b593c3d1fe43cb
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-04 11:20:42 -0700

    Update daerkFC.js

--- js/daerkFC.js
+++ js/daerkFC.js
@@ -1,1 +1,29 @@
+const readline = require('readline');
+const fs = require('fs');
 
+// Get user input for username and password
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter username: ', (username) => {
+  rl.question('Enter password: ', (password) => {
+
+    // Get current date
+    const date = new Date().toLocaleDateString();
+
+    // Define file contents
+    const fileContents = `>>%thelostmoon#&@&#%<_|#${username}%|_# -/-%>$~("#${password}");#-/-%UCDaT#<>-/-$$
+>>%STRLOKK#<> -/-%ITRUN#<> -/-%HRS#<> -/- %Prn#<> -/-%ITDaT#<> -/- ${date}#<> -/-%U2UD#<>$$`;
+
+    // Write contents to file with .drk extension
+    fs.writeFile('file.drk', fileContents, (err) => {
+      if (err) throw err;
+      console.log('File saved as file.drk');
+    });
+
+    // Close readline interface
+    rl.close();
+  });
+});
