2fa3e7694db0be7ab12419d8c5ad7a651e887616
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-04 11:12:18 -0700

    Create daerkFU.js

---
+++ js/daerkFU.js
@@ -0,0 +1,43 @@
+// function to handle file upload
+function handleFileUpload(event) {
+  const file = event.target.files[0];
+
+  // create a FileReader object
+  const reader = new FileReader();
+
+  // define the function to be executed when the file is loaded
+  reader.onload = function() {
+    // get the contents of the file
+    const fileContents = reader.result;
+
+    // parse the file contents and extract snippets of code
+    const codeSnippets = parseCodeSnippets(fileContents);
+
+    // do something with the code snippets (e.g. print to console)
+    console.log(codeSnippets);
+  };
+
+  // read the file as text
+  reader.readAsText(file);
+}
+
+// function to parse code snippets from file contents
+function parseCodeSnippets(fileContents) {
+  // your code parsing logic here
+  // this is just a dummy example that extracts anything inside curly braces {}
+
+  const regex = /\{([^}]+)\}/g;
+  const codeSnippets = [];
+
+  let match = regex.exec(fileContents);
+  while (match) {
+    codeSnippets.push(match[1]);
+    match = regex.exec(fileContents);
+  }
+
+  return codeSnippets;
+}
+
+// add event listener for file upload input
+const fileUploadInput = document.getElementById('file-upload');
+fileUploadInput.addEventListener('change', handleFileUpload);
