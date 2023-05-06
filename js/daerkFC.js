7b01617cd75b4411a70b386db6a4cdcc929f6bf0
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-05 19:41:36 -0700

    Update daerkFC.js

--- js/daerkFC.js
+++ js/daerkFC.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function createDrkFile() {
   const username = prompt('Enter username:');
   const password = prompt('Enter password:');
+  const fileName = prompt('Enter file name:');
   const date = new Date().toLocaleDateString();
 
   const fileContents = `>>%thelostmoon#&@&#%<_|#${username}%|_# -/-%>$~("#${password}");#-/-%UCDaT#<>-/-$$
@@ -16,7 +17,7 @@ function createDrkFile() {
   const blob = new Blob([fileContents], { type: 'text/plain' });
   const anchor = document.createElement('a');
   anchor.href = URL.createObjectURL(blob);
-  anchor.download = 'file.drk';
+  anchor.download = `${fileName}.drk`;
   anchor.style.display = 'none';
   document.body.appendChild(anchor);
   anchor.click();
