de69e8763bed735092a4f3da71400c620a528ce5
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-05 19:29:13 -0700

    Update daerkFC.js

--- js/daerkFC.js
+++ js/daerkFC.js
@@ -1,29 +1,21 @@
-const readline = require('readline');
-const fs = require('fs');
+// daerkFC.js
 
-// Get user input for username and password
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+document.addEventListener('DOMContentLoaded', function() {
+  const createButton = document.getElementById('create-file-btn');
+  createButton.addEventListener('click', createDrkFile);
 });
 
-rl.question('Enter username: ', (username) => {
-  rl.question('Enter password: ', (password) => {
+function createDrkFile() {
+  const username = prompt('Enter username:');
+  const password = prompt('Enter password:');
+  const date = new Date().toLocaleDateString();
 
-    // Get current date
-    const date = new Date().toLocaleDateString();
-
-    // Define file contents
-    const fileContents = `>>%thelostmoon#&@&#%<_|#${username}%|_# -/-%>$~("#${password}");#-/-%UCDaT#<>-/-$$
+  const fileContents = `>>%thelostmoon#&@&#%<_|#${username}%|_# -/-%>$~("#${password}");#-/-%UCDaT#<>-/-$$
 >>%STRLOKK#<> -/-%ITRUN#<> -/-%HRS#<> -/- %Prn#<> -/-%ITDaT#<> -/- ${date}#<> -/-%U2UD#<>$$`;
 
-    // Write contents to file with .drk extension
-    fs.writeFile('file.drk', fileContents, (err) => {
-      if (err) throw err;
-      console.log('File saved as file.drk');
-    });
-
-    // Close readline interface
-    rl.close();
-  });
-});
+  const blob = new Blob([fileContents], { type: 'text/plain' });
+  const anchor = document.createElement('a');
+  anchor.href = URL.createObjectURL(blob);
+  anchor.download = 'file.drk';
+  anchor.click();
+}
