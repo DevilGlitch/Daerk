76e89a7ebc321e226e475d1d9f34846ad26348dd
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-05-16 00:22:56 +0000

    dynamic update

--- js/daerkFU.js
+++ js/daerkFU.js
@@ -9,13 +9,18 @@ function handleFileUpload(event) {
   reader.onload = function() {
     // get the contents of the file
     const fileContents = reader.result;
-	  
-	fileContents = decodeURIComponent(escape(atob(fileContents)));
-    // parse the file contents and extract snippets of code
-    const codeSnippets = parseCodeSnippets(fileContents);
 
-    // do something with the code snippets (e.g. print to console)
-    console.log(codeSnippets);
+    // decode the file contents from base64
+    const decodedContents = atob(fileContents);
+
+    // parse the file contents and extract snippets of code
+    const codeSnippets = parseCodeSnippets(decodedContents);
+
+    // do something with the code snippets (e.g. add to a global data array)
+    addToDataArray(codeSnippets);
+
+    // print the updated data array
+    console.log(dataArray);
   };
 
   // read the file as text
@@ -24,19 +29,28 @@ function handleFileUpload(event) {
 
 // function to parse code snippets from file contents
 function parseCodeSnippets(fileContents) {
-  // your code parsing logic here
-  // this is just a dummy example that extracts anything inside curly braces {}
-
-  const regex = /\{([^}]+)\}/g;
+  const regex = /%<_\|#([\s\S]*?)#_\|%>/g;
   const codeSnippets = [];
 
   let match = regex.exec(fileContents);
   while (match) {
-    codeSnippets.push(match[1]);
+    const snippet = match[1].trim(); // Trim whitespace from the extracted snippet
+    codeSnippets.push(snippet);
     match = regex.exec(fileContents);
   }
 
+  codeSnippets.forEach((snippet, index) => {
+    console.log(`Code Snippet ${index + 1}: ${snippet}`);
+  });
+
   return codeSnippets;
+}
+
+// function to add code snippets to a global data array
+function addToDataArray(codeSnippets) {
+  // your logic to add code snippets to a global data array here
+  // for example:
+  globalDataArray.push(...codeSnippets);
 }
 
 // add event listener for file upload input
